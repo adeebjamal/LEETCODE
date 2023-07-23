090f8c5ac2b9e6fc4929374ed526b9d4cdad1fd8
Adeeb Jamal <adeeb.jamal186@outlook.com>
2023-07-23 23:36:26 +0530

    23-07-2023 23:36

--- LEETCODE-1261.cpp
+++ LEETCODE-1261.cpp
@@ -11,6 +11,36 @@ public:
         this->right=NULL;
     }
 };
+class FindElements2 {
+public:
+    TreeNode* root = NULL;
+    unordered_map<int,int> mp;
+    void solve(TreeNode* root, int current, bool left) {
+        if(root == NULL) {
+            return;
+        }
+        if(left == true) {
+            root->val = 2 * current + 1;
+        }
+        else if(left == false) {
+            root->val = 2 * current + 2;
+        }
+        mp[root->val]++;
+        solve(root->left, root->val, true);
+        solve(root->right, root->val, false);
+    }
+    FindElements2(TreeNode* root) {
+        root->val = 0;
+        this->root = root;
+        mp[0]++;
+        solve(root->left,root->val,true);
+        solve(root->right,root->val,false);
+    }
+    
+    bool find(int target) {
+        return mp[target] > 0;
+    }
+};
 class FindElements {
 public:
     TreeNode* node;
