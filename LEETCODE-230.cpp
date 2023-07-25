25d442658b5b3880fa1351036a5e4f9258c900e8
Adeeb Jamal <adeeb.jamal186@outlook.com>
2023-07-25 22:38:00 +0530

    25-07-2023 22:37

---
+++ LEETCODE-230.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+    void dfs(TreeNode* root, priority_queue<int,vector<int>,greater<int>>& pq) {
+        if(root == NULL) {
+            return;
+        }
+        pq.push(root->val);
+        dfs(root->left,pq);
+        dfs(root->right,pq);
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        priority_queue<int,vector<int>,greater<int>> pq;
+        dfs(root,pq);
+        k--;
+        while(k--) {
+            pq.pop();
+        }
+        return pq.top();
+    }
+};
