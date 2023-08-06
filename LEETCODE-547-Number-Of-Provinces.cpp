dd56718a251dffd841ddd54abf9e45cc1195d299
Adeeb Jamal <adeeb.jamal186@outlook.com>
2023-08-06 14:43:25 +0530

    06-08-2023 14:43

--- LEETCODE-547-Number-Of-Provinces.cpp
+++ LEETCODE-547-Number-Of-Provinces.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-class Solution {
+class Solution2 {
 public:
     void bfs(int& source, vector<vector<int>>& graph, vector<bool>& vis) {
         queue<int> q;
@@ -25,6 +25,52 @@ public:
             if(vis[i] == false) {
                 answer++;
                 this->bfs(i,isConnected,vis);
+            }
+        }
+        return answer;
+    }
+};
+class Solution {
+public:
+    void dfs(int& source, unordered_map<int,vector<int>>& graph, vector<bool>& visited) {
+        visited[source] = true;
+        for(int& neighbour: graph[source]) {
+            if(visited[neighbour] == false) {
+                this->dfs(neighbour,graph,visited);
+            }
+        }
+    }
+    void bfs(int& source, unordered_map<int,vector<int>>& graph, vector<bool>& visited) {
+        queue<int> q;
+        q.push(source);
+        visited[source] = true;
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+            for(int& neighbour: graph[node]) {
+                if(visited[neighbour] == false) {
+                    q.push(neighbour);
+                    visited[neighbour] = true;
+                }
+            }
+        }
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        unordered_map<int,vector<int>> graph;
+        for(int i=0; i<isConnected.size(); i++) {
+            for(int j=0; j<isConnected[0].size(); j++) {
+                if(isConnected[i][j] == 1) {
+                    graph[i].push_back(j);
+                    graph[j].push_back(i);
+                }
+            }
+        }
+        int nodes = isConnected.size(), answer = 0;
+        vector<bool> visited(nodes,false);
+        for(int node=0; node<nodes; node++) {
+            if(visited[node] == false) {
+                answer++;
+                this->bfs(node,graph,visited);
             }
         }
         return answer;
