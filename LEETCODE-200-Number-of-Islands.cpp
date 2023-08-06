4c800c9028d2f88158ac109901b108c93f23df57
Adeeb Jamal <adeeb.jamal186@outlook.com>
2023-08-06 15:31:40 +0530

    06-08-2023 15:31

--- LEETCODE-200-Number-of-Islands.cpp
+++ LEETCODE-200-Number-of-Islands.cpp
@@ -1,38 +1,48 @@
-// https://leetcode.com/problems/number-of-islands/
+// https://leetcode.com/problems/number-of-islands/description/
 #include<bits/stdc++.h>
 using namespace std;
+int dr[] = {-1,0,1,0};
+int dc[] = {0,-1,0,1};
 class Solution {
 public:
-    void bfs(int& row, int& col, vector<vector<bool>>& visited, vector<vector<char>>& grid) {
-        visited[row][col] = true;
+    void bfs(int& row, int& col, vector<vector<char>>& grid, vector<vector<bool>>& visited) {
         queue<pair<int,int>> q;
         q.push({row,col});
+        visited[row][col] = true;
         while(!q.empty()) {
             int curr_row = q.front().first, curr_col = q.front().second;
             q.pop();
-            for(int delrow = -1; delrow <= 1; delrow++) {
-                for(int delcol = -1; delcol <= 1; delcol++) {
-                    int newrow = curr_row + delrow, newcol = curr_col + delcol;
-                    if(newrow>=0 and newrow<grid.size() and newcol>=0 and newcol<grid[0].size()) {
-                        if(!visited[newrow][newcol] and grid[newrow][newcol] == '1') {
-                            if(!(newrow != curr_row and newcol != curr_col)) {
-                                visited[newrow][newcol] = true;
-                                q.push({newrow,newcol});
-                            }
-                        }
+            for(int i=0; i<4; i++) {
+                int newrow = curr_row + dr[i], newcol = curr_col + dc[i];
+                if(newrow >= 0 and newrow < grid.size() and newcol >= 0 and newcol < grid[0].size()) {
+                    if(visited[newrow][newcol] == false and grid[newrow][newcol] == '1') {
+                        q.push({newrow,newcol});
+                        visited[newrow][newcol] = true;
                     }
+                }
+            }
+        }
+    }
+    void dfs(int& row, int& col, vector<vector<char>>& grid, vector<vector<bool>>& visited) {
+        visited[row][col] = true;
+        for(int i=0; i<4; i++) {
+            int newrow = row + dr[i], newcol = col + dc[i];
+            if(newrow >= 0 and newrow < grid.size() and newcol >= 0 and newcol < grid[0].size()) {
+                if(visited[newrow][newcol] == false and grid[newrow][newcol] == '1') {
+                    this->dfs(newrow,newcol,grid,visited);
                 }
             }
         }
     }
     int numIslands(vector<vector<char>>& grid) {
         vector<vector<bool>> visited(grid.size(),vector<bool>(grid[0].size(),false));
-        int count=0;
+        int count = 0;
         for(int row=0; row<grid.size(); row++) {
             for(int col=0; col<grid[0].size(); col++) {
                 if(visited[row][col] == false and grid[row][col] == '1') {
                     count++;
-                    bfs(row, col, visited, grid);
+                    this->dfs(row,col,grid,visited);
+                    // this->bfs(row,col,grid,visited);
                 }
             }
         }
