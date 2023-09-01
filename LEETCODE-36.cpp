14ea5563efafa149aaf65d821542ffd8352d31ed
Adeeb Jamal <adeeb.jamal186@outlook.com>
2023-09-01 19:10:50 +0530

    01-09-2023 19:10

--- LEETCODE-36.cpp
+++ LEETCODE-36.cpp
@@ -3,40 +3,25 @@ using namespace std;
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        for(vector<char>& row: board) {
-            unordered_map<char,int> mp;
-            for(char& digit: row) {
-                if(digit != '.') {
-                    mp[digit]++;
+        unordered_map<int,unordered_set<char>> rows, cols;
+        map<pair<int,int>,unordered_set<char>> squares;
+        for(int r=0; r<9; r++) {
+            for(int c=0; c<9; c++) {
+                if(board[r][c] == '.') {
+                    continue;
                 }
-                if(mp[digit] > 1) {
+                if(rows[r].find(board[r][c]) != rows[r].end()) {
                     return false;
                 }
-            }
-        }
-        for(int j=0; j<board[0].size(); j++) {
-            unordered_map<char,int> mp;
-            for(vector<char>& row: board) {
-                if(row[j] != '.') {
-                    mp[row[j]]++;
-                }
-                if(mp[row[j]] > 1) {
+                if(cols[c].find(board[r][c]) != cols[c].end()) {
                     return false;
                 }
-            }
-        }
-        vector<pair<int,int>> vec = {{0,0},{0,3},{0,6},{3,0},{3,3},{3,6},{6,0},{6,3},{6,6}};
-        for(pair<int,int>& p: vec) {
-            for(int i=p.first; i<p.first+3; i++) {
-                unordered_map<char,int> mp;
-                for(int j=p.second; j<p.second+3; j++) {
-                    if(board[i][j] != '.') {
-                        mp[board[i][j]]++;
-                    }
-                    if(mp[board[i][j]] > 1) {
-                        return false;
-                    }
+                if(squares[{r/3,c/3}].find(board[r][c]) != squares[{r/3,c/3}].end()) {
+                    return false;
                 }
+                rows[r].insert(board[r][c]);
+                cols[c].insert(board[r][c]);
+                squares[{r/3,c/3}].insert(board[r][c]);
             }
         }
         return true;
