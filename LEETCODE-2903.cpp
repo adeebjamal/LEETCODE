ebfca205a7f1e6775c7096e5d7bdcd0e154113ec
Adeeb Jamal <adeeb.jamal186@outlook.com>
2023-10-16 13:57:00 +0530

    16-10-2023 13:56

---
+++ LEETCODE-2903.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> findIndices(vector<int>& nums, int indexDifference, int valueDifference) {
+        for(int i=0; i<nums.size(); i++) {
+            for(int j=i; j<nums.size(); j++) {
+                if(abs(i-j) >= indexDifference and abs(nums[i]-nums[j]) >= valueDifference) {
+                    return {i,j};
+                }
+            }
+        }
+        return {-1,-1};
+    }
+};
