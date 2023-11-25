e5237f00744d08ee1b7687517416dd46c4b143a7
Adeeb Jamal <adeeb.jamal186@outlook.com>
2023-11-25 15:44:03 +0530

    solved Sort array of 0,1,2

---
+++ LEETCODE_75_Sort_Colors.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low = 0, medium = 0, high = nums.size()-1;
+        while(medium <= high) {
+            if(nums[medium] == 0) {
+                swap(nums[low++], nums[medium++]);
+            }
+            else if(nums[medium] == 1) {
+                medium++;
+            }
+            else if(nums[medium] == 2) {
+                swap(nums[medium], nums[high--]);
+            }
+        }
+    }
+};
