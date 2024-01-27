216a4f301a777f8f79295bfa4e78f7515b0ae0dd
Adeeb Jamal <adeeb.jamal186@outlook.com>
2024-01-27 20:23:38 +0530

    solved LEETCODE 576

---
+++ 576_Out_of_Boundary_Paths.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+    int max_steps = 0;
+    unordered_map<string, int> dp;
+    int f(int row, int col, int steps, int& rows, int& cols) {
+        if (row < 0 or col < 0 or row >= rows or col >= cols) {
+            return 1;
+        }
+        if (steps >= this->max_steps) {
+            return 0;
+        }
+        string key = to_string(row) + "_" + to_string(col) + "_" + to_string(steps);
+        if (dp.find(key) != dp.end()) {
+            return dp[key];
+        }
+        int mod = 1e9 + 7;
+        int up = this->f(row - 1, col, steps + 1, rows, cols);
+        int down = this->f(row + 1, col, steps + 1, rows, cols);
+        int left = this->f(row, col - 1, steps + 1, rows, cols);
+        int right = this->f(row, col + 1, steps + 1, rows, cols);
+        return dp[key] = ((up % mod + down % mod) % mod + (left % mod + right % mod) % mod) % mod;
+    }
+    public:
+    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        this->max_steps = maxMove;
+        return f(startRow, startColumn, 0, m, n);
+    }
+};
