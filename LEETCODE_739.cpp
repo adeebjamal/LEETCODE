cea923988932dea6f5ea5a3e862262277cafcb5e
Adeeb Jamal <adeeb.jamal186@outlook.com>
2024-01-31 20:57:38 +0530

    LEETCODE 739

---
+++ LEETCODE_739.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int length = temperatures.size();
+        vector<int> answer(length, 0);
+        stack<pair<int,int>> st;
+        for(int i = 0; i < length; i++) {
+            if(st.empty()) {
+                st.push({temperatures[i], i});
+                continue;
+            }
+            while(st.top().first < temperatures[i]) {
+                answer[st.top().second] = i - st.top().second;
+                st.pop();
+            }
+            st.push({temperatures[i], i});
+        }
+        return answer;
+    }
+};
